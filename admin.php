de863dbd6ba2ee8aa85ea6d6ebdfa48ecc32c451
Giuliano Ascierto <giulianoascierto@gmail.com>
2021-07-27 12:50:47 +0200

    Aggiunta data chiusura votazione e formattata data

--- admin.php
+++ admin.php
@@ -18,6 +18,7 @@
                   <th scope="col">Visibilità</th>
                   <th scope="col">Stato</th> 
                   <th scope="col">Creata il</th>
+                  <th scope="col">Da chiudere il</th>
                   <th scope="col">Vedi risultati live</th>
                   <th scope="col">Chiudi Votazione</th>
                   <th scope="col">Elimina Votazione</th>
@@ -30,7 +31,8 @@
                   <td><?php echo $poll['name_poll'] ?></td>
                   <td><?php $poll['is_private']== 0 ? printf('Pubblica'):printf('Privata') ?></td>
                   <td><?php $poll['is_closed']== 0 ? printf('🔴'):printf('🟢') ?></td>
-                  <td><?php echo implode('-', array_reverse(explode('-',$poll['created_at']))) ?></td> 
+                  <td><?php $date =new DateTime($poll['created_at']); echo $date->format('d-m-y H:i:s'); ?></td>
+                  <td><?php echo implode('-', array_reverse(explode('-',$poll['closing_day']))) ?></td>  
                   <td><a href="./live-results.php?id=<?php echo $poll['id'] ?>" class="btn btn-outline-warning">GO!</a></td>
                   <td><a href="./includes/close-poll.php?id=<?php echo $poll['id'] ?>" class="btn btn-outline-danger">✔️</a></td>
                   <td><a href="./includes/delete-poll.php?id=<?php echo $poll['id'] ?>" class="btn btn-danger">❌</a></td>
